f2e91e2aaf41a03bfa287049077adedbdb93abd0
Boi Hoanh <122199781+rein-desu999@users.noreply.github.com>
2026-03-04 17:55:47 -0800

    Refactor jetson_go.c for improved readability and functionality

--- jetson_go.c
+++ jetson_go.c
@@ -1,9 +1,9 @@
-// jetson_go.c
-// Build:  gcc -O2 -Wall jetson_go.c -o jetson_go
-// Run:    ./jetson_go
+// jetson_cmd.c
+// Build:  gcc -O2 -Wall jetson_cmd.c -o jetson_cmd
+// Run:    sudo ./jetson_cmd
 //
-// Opens /dev/ttyTHS1 @115200 8N1, sends "GO\n", then prints any STM32 replies.
-// Press 's' + Enter to send STOP, 'g' + Enter to send GO again, 'q' to quit.
+// Type commands like: GO, FWD, BWD, STOP then press Enter.
+// It will send "<YOURTEXT>\n" to /dev/ttyTHS1 and print any STM32 replies.
 
 #include <errno.h>
 #include <fcntl.h>
@@ -13,55 +13,39 @@
 #include <unistd.h>
 #include <sys/select.h>
 
-static int set_serial(int fd, int baud)
+static int set_serial(int fd)
 {
   struct termios tty;
-  if (tcgetattr(fd, &tty) != 0) {
-    perror("tcgetattr");
-    return -1;
-  }
+  if (tcgetattr(fd, &tty) != 0) { perror("tcgetattr"); return -1; }
 
   cfmakeraw(&tty);
 
-  // 8N1
   tty.c_cflag &= ~PARENB;
   tty.c_cflag &= ~CSTOPB;
   tty.c_cflag &= ~CSIZE;
   tty.c_cflag |= CS8;
 
   tty.c_cflag |= (CLOCAL | CREAD);
-  tty.c_cflag &= ~CRTSCTS;      // no HW flow control
+  tty.c_cflag &= ~CRTSCTS;
 
-  // Non-blocking reads with select()
   tty.c_cc[VMIN]  = 0;
   tty.c_cc[VTIME] = 0;
 
-  speed_t spd = B115200;
-  (void)baud;
-  cfsetispeed(&tty, spd);
-  cfsetospeed(&tty, spd);
+  cfsetispeed(&tty, B115200);
+  cfsetospeed(&tty, B115200);
 
-  if (tcsetattr(fd, TCSANOW, &tty) != 0) {
-    perror("tcsetattr");
-    return -1;
-  }
-
+  if (tcsetattr(fd, TCSANOW, &tty) != 0) { perror("tcsetattr"); return -1; }
   tcflush(fd, TCIOFLUSH);
   return 0;
 }
 
-static int write_all(int fd, const char *s)
+static int write_all(int fd, const char *buf, size_t len)
 {
-  size_t n = strlen(s);
-  while (n) {
-    ssize_t w = write(fd, s, n);
-    if (w < 0) {
-      if (errno == EINTR) continue;
-      perror("write");
-      return -1;
-    }
-    s += (size_t)w;
-    n -= (size_t)w;
+  while (len) {
+    ssize_t w = write(fd, buf, len);
+    if (w < 0) { if (errno == EINTR) continue; perror("write"); return -1; }
+    buf += (size_t)w;
+    len -= (size_t)w;
   }
   return 0;
 }
@@ -70,68 +54,46 @@ int main(void)
 {
   const char *dev = "/dev/ttyTHS1";
   int fd = open(dev, O_RDWR | O_NOCTTY | O_NONBLOCK);
-  if (fd < 0) {
-    perror("open /dev/ttyTHS1");
-    return 1;
-  }
-
-  if (set_serial(fd, 115200) != 0) {
-    close(fd);
-    return 1;
-  }
+  if (fd < 0) { perror("open /dev/ttyTHS1"); return 1; }
+  if (set_serial(fd) != 0) { close(fd); return 1; }
 
   printf("Opened %s @115200 8N1\n", dev);
+  printf("Type command then Enter (GO/FWD/BWD/STOP). Type QUIT to exit.\n\n");
 
-  // Send GO
-  if (write_all(fd, "GO\n") != 0) {
-    close(fd);
-    return 1;
-  }
-  printf("Sent: GO\\n\n");
-  printf("Commands: g=GO, s=STOP, q=quit (press key then Enter)\n\n");
-
-  char rxbuf[256];
-  char inbuf[64];
+  char rx[256];
+  char line[128];
 
   while (1) {
     fd_set rset;
     FD_ZERO(&rset);
     FD_SET(fd, &rset);
     FD_SET(STDIN_FILENO, &rset);
+    int maxfd = fd > STDIN_FILENO ? fd : STDIN_FILENO;
 
-    int maxfd = (fd > STDIN_FILENO) ? fd : STDIN_FILENO;
-    struct timeval tv;
-    tv.tv_sec = 0;
-    tv.tv_usec = 200000; // 200ms
-
+    struct timeval tv = { .tv_sec = 0, .tv_usec = 200000 };
     int r = select(maxfd + 1, &rset, NULL, NULL, &tv);
-    if (r < 0) {
-      if (errno == EINTR) continue;
-      perror("select");
-      break;
-    }
+    if (r < 0) { if (errno == EINTR) continue; perror("select"); break; }
 
     if (FD_ISSET(fd, &rset)) {
-      ssize_t n = read(fd, rxbuf, sizeof(rxbuf) - 1);
-      if (n > 0) {
-        rxbuf[n] = '\0';
-        printf("STM32: %s", rxbuf); // STM32 prints \r\n
-        fflush(stdout);
-      }
+      ssize_t n = read(fd, rx, sizeof(rx) - 1);
+      if (n > 0) { rx[n] = 0; printf("STM32: %s", rx); fflush(stdout); }
     }
 
     if (FD_ISSET(STDIN_FILENO, &rset)) {
-      if (!fgets(inbuf, sizeof(inbuf), stdin)) break;
-      if (inbuf[0] == 'q') break;
-      if (inbuf[0] == 'g') {
-        write_all(fd, "GO\n");
-        printf("Sent: GO\\n\n");
-      } else if (inbuf[0] == 's') {
-        write_all(fd, "STOP\n");
-        printf("Sent: STOP\\n\n");
-      } else {
-        printf("Unknown. Use g/s/q.\n");
-      }
+      if (!fgets(line, sizeof(line), stdin)) break;
+
+      // remove trailing newline
+      size_t L = strlen(line);
+      while (L && (line[L-1] == '\n' || line[L-1] == '\r')) line[--L] = 0;
+
+      if (strcasecmp(line, "QUIT") == 0) break;
+      if (L == 0) continue;
+
+      // send command + '\n'
+      char out[160];
+      snprintf(out, sizeof(out), "%s\n", line);
+      if (write_all(fd, out, strlen(out)) != 0) break;
+      printf("Sent: %s\\n\n", line);
     }
   }
 
