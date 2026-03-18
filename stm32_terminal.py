fd9b3b644bd8d894cbb0285b301e404bf68a3198
walee12311231 <lol12345walker@gmail.com>
2026-03-17 18:10:50 -0700

    Update stm32_terminal.py

--- stm32_terminal.py
+++ stm32_terminal.py
@@ -16,7 +16,7 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from collections import deque
 
 # ─── Config ───────────────────────────────────────────────────────────────────
-DEFAULT_BAUD = 115200
+DEFAULT_BAUD = 9600
 WEB_PORT     = 8765
 LOG_MAXLEN   = 500
 
